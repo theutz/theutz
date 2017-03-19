7e915512b87b200cd5a0fd24d634322c33715cc0
Michael Utz <michaelutz@utzMBP.local>
2017-03-18 22:22:08 -0500

    Banner image with title responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BannerComponent
+    BannerComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
