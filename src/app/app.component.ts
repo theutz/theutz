324094bbada53cd128f31ef274fe57ab88f50037
Michael Utz <michaelutz@utzMBP.local>
2017-03-18 23:04:32 -0500

    Fixed pipe so it will always break on spaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  title = 'Michael Utz';
+
+  constructor(
+    private _ts: Title
+  ) { }
+
+  ngOnInit() {
+    this._ts.setTitle(this.title);
+  }
 }
--- src/app/space-to-br.pipe.ts
+++ src/app/space-to-br.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SpaceToBrPipe implements PipeTransform {
 
   transform(value: string, args?: any): any {
-    return value.replace(' ', '<br>');
+    return value.split(' ').join('<br>');
   }
 
 }
