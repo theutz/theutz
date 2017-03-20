6942fe782cb466eca89177ca2b3c2f83c01c6570
Michael Utz <michaelutz@utzMBP.local>
2017-03-19 19:17:33 -0500

    Fancied up the banner

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BannerComponent implements OnInit {
   @Input() title: string;
+  roles: string[] = ['Developer', 'Designer'];
 
   constructor() { }
 
