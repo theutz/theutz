0e0555bb7b41747aabc6707c391a8ecc47ccd2ee
Michael Utz <michaelutz@utzMBP.local>
2017-03-20 21:38:16 -0500

    Work history descriptions first draft

--- src/app/work-history.service.ts
+++ src/app/work-history.service.ts
@@ -17,7 +17,7 @@ export class WorkHistoryService {
     {
       company: 'Uber/Lyft',
       title: 'Driver',
-      description: '',
+      description: 'Set out for some advenutre as a self-employed driver in one of the nation\'s fastest growing cities.',
       startDate: new Date(2015, 5),
       endDate: new Date(2016, 1),
       city: 'Nashville',
@@ -26,16 +26,18 @@ export class WorkHistoryService {
     {
       company: 'Emergency Reporting',
       title: 'Content Writer/Producer',
-      description: '',
+      /* tslint:disable-next-line:max-line-length */
+      description: 'Content marketing and front-end web development. Built <a href="http://emergencyreporting.com" target="_blank">emergencyreporting.com</a> with Wordpress.',
       startDate: new Date(2013, 6),
       endDate: new Date(2015, 2),
       city: 'Bellingham',
       state: 'Washington'
     },
     {
-      company: 'Christ United Methodist Church',
+      company: 'Christ Church',
       title: 'Creative Director',
-      description: '',
+      /* tslint:disable-next-line:max-line-length */
+      description: 'Supervised music, media, and web services for the congregation. Built <a href="http://christchurchdavenport.org">christchurchdavenport.org</a> on Squarespace.',
       startDate: new Date(2012, 5),
       endDate: new Date(2013, 1),
       city: 'Davenport',
@@ -44,7 +46,7 @@ export class WorkHistoryService {
     {
       company: 'SimpleDelta, LLC',
       title: 'Vice-President/Co-Founder',
-      description: '',
+      description: 'Co-founded a company trying to build a web app to help independent auto mechanics offer services online.',
       startDate: new Date(2010, 11),
       endDate: new Date(2012, 5),
       city: 'Bellingham',
@@ -53,7 +55,7 @@ export class WorkHistoryService {
     {
       company: 'ShareNow, Inc',
       title: 'Project Coordinator',
-      description: '',
+      description: 'Joined a startup looking to create social media strategies for non-profits, working on multiple projects.',
       startDate: new Date(2009, 1),
       endDate: new Date(2010, 7),
       city: 'Bellingham',
@@ -62,7 +64,7 @@ export class WorkHistoryService {
     {
       company: 'Northwest University',
       title: 'Associate Director of Media Services & Adjunct Faculty',
-      description: '',
+      description: 'Oversee all recorded media for Northwest University, producing multiple full-length albums. Taught a music technology course.',
       startDate: new Date(2007, 7),
       endDate: new Date(2009, 5),
       city: 'Kirkland',
@@ -71,7 +73,7 @@ export class WorkHistoryService {
     {
       company: 'Courtyard by Marriott',
       title: 'Front Desk Supervisor',
-      description: '',
+      description: 'Helped with front-office operations, working with other agents to ensure an excellent customer service for the guest.',
       startDate: new Date(2006, 10),
       endDate: new Date(2007, 7),
       city: 'Kirkland',
