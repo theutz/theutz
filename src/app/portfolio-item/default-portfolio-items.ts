821aa000fb494626ddb62279c68d2d878a8e0485
Michael Utz <michaelutz@utzMBP.local>
2017-03-26 18:52:43 -0500

    Responsive portfolio items

--- src/app/portfolio-item/default-portfolio-items.ts
+++ src/app/portfolio-item/default-portfolio-items.ts
@@ -4,17 +4,20 @@ export const defaultPortfolioItems: PortfolioItem[] = [
     {
         title: 'Reservationista',
         url: 'http://reservationista.com',
+        description: 'Work-in-progress to make selling easier at Loews.',
         thumbnailUrl: '/assets/reservationista.png',
         githubUrl: 'https://github.com/theutz/reservationista.git'
     },
     {
         title: 'Emergency Reporting',
         url: 'http://emergencyreporting.com',
+        description: 'Developed site from WordPress, Bootstrap 3, etc.',
         thumbnailUrl: '/assets/emergency-reporting.png'
     },
     {
         title: 'Christ Church',
         url: 'http://christchurchdavenport.org',
+        description: 'Designed simple Squarespace site for the church to use.',
         thumbnailUrl: '/assets/christ-church.png'
     }
 ];
