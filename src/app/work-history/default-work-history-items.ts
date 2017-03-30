a0ebf738f645328c5231608ea23ec3de264c0208
Michael Utz <michaelutz@utzMBP.local>
2017-03-29 20:53:40 -0500

    Fixed errant dates

--- src/app/work-history/default-work-history-items.ts
+++ src/app/work-history/default-work-history-items.ts
@@ -5,7 +5,7 @@ export const defaultWorkHistoryItems: WorkHistoryItem[] = [
         title: 'Travel Planner',
         /* tslint:disable-next-line:max-line-length */
         description: 'Reservations agent at <a href="http://loewshotels.com" target="_blank">Loews Hotels</a> (NYSE: L). Among the top agents in both sales numbers and service measures.',
-        startDate: new Date(2015, 12),
+        startDate: new Date(2015, 11),
         endDate: new Date(),
         city: 'Nashville',
         state: 'Tennessee'
@@ -15,8 +15,8 @@ export const defaultWorkHistoryItems: WorkHistoryItem[] = [
         title: 'Driver',
         /* tslint:disable-next-line:max-line-length */
         description: 'Set out for some advenutre as a self-employed Uber & Lyft driver in one of the nation\'s fastest growing cities.',
-        startDate: new Date(2015, 5),
-        endDate: new Date(2016, 1),
+        startDate: new Date(2015, 4),
+        endDate: new Date(2015, 11),
         city: 'Nashville',
         state: 'Tennessee'
     },
@@ -25,8 +25,8 @@ export const defaultWorkHistoryItems: WorkHistoryItem[] = [
         title: 'Content Writer/Producer',
         /* tslint:disable-next-line:max-line-length */
         description: 'Content marketing and front-end web development. Built <a href="http://emergencyreporting.com" target="_blank">emergencyreporting.com</a> with Wordpress.',
-        startDate: new Date(2013, 6),
-        endDate: new Date(2015, 2),
+        startDate: new Date(2013, 5),
+        endDate: new Date(2015, 4),
         city: 'Bellingham',
         state: 'Washington'
     },
@@ -35,8 +35,8 @@ export const defaultWorkHistoryItems: WorkHistoryItem[] = [
         title: 'Creative Director',
         /* tslint:disable-next-line:max-line-length */
         description: 'Supervised music, media, and web services for the congregation. Built <a href="http://christchurchdavenport.org" target="_blank">christchurchdavenport.org</a> on Squarespace.',
-        startDate: new Date(2012, 5),
-        endDate: new Date(2013, 1),
+        startDate: new Date(2012, 0),
+        endDate: new Date(2013, 4),
         city: 'Davenport',
         state: 'Iowa'
     },
@@ -44,8 +44,8 @@ export const defaultWorkHistoryItems: WorkHistoryItem[] = [
         company: 'SimpleDelta, LLC',
         title: 'Vice-President/Co-Founder',
         description: 'Co-founded a company trying to build a web app to help independent auto mechanics offer services online.',
-        startDate: new Date(2010, 11),
-        endDate: new Date(2012, 5),
+        startDate: new Date(2010, 10),
+        endDate: new Date(2012, 0),
         city: 'Bellingham',
         state: 'Washington'
     },
@@ -53,8 +53,8 @@ export const defaultWorkHistoryItems: WorkHistoryItem[] = [
         company: 'ShareNow, Inc',
         title: 'Project Coordinator',
         description: 'Joined a startup looking to create social media strategies for non-profits, working on multiple projects.',
-        startDate: new Date(2009, 1),
-        endDate: new Date(2010, 7),
+        startDate: new Date(2009, 11),
+        endDate: new Date(2010, 6),
         city: 'Bellingham',
         state: 'Washington'
     },
@@ -63,8 +63,8 @@ export const defaultWorkHistoryItems: WorkHistoryItem[] = [
         title: 'Associate Director of Media Services & Adjunct Faculty',
         /* tslint:disable-next-line:max-line-length */
         description: 'Oversee all recorded media for Northwest University, producing multiple full-length albums. Taught a music technology course.',
-        startDate: new Date(2007, 7),
-        endDate: new Date(2009, 5),
+        startDate: new Date(2007, 6),
+        endDate: new Date(2009, 4),
         city: 'Kirkland',
         state: 'Washington'
     },
@@ -73,8 +73,8 @@ export const defaultWorkHistoryItems: WorkHistoryItem[] = [
         title: 'Front Desk Supervisor',
         /* tslint:disable-next-line:max-line-length */
         description: 'Helped with front-office operations, working with other agents to ensure an excellent customer service for the guest.',
-        startDate: new Date(2006, 10),
-        endDate: new Date(2007, 7),
+        startDate: new Date(2006, 9),
+        endDate: new Date(2007, 6),
         city: 'Kirkland',
         state: 'Washington'
     }
