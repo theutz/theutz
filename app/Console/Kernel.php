b471a8124ec79cce9f2ccabbf9e31c2873e022f6
Michael Utz <michael@theutz.com>
2022-06-29 17:58:30 +0300

    Setup cronless scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('telescope:prune --hours=48')->daily();
+        $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
 
     /**
@@ -25,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
