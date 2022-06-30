66237438f79f06f2df72f899cb0736fc3d03ecf9
Michael Utz <michael@theutz.com>
2022-06-30 15:38:08 +0300

    Browsersync blade
    Signed-off-by: Michael Utz <michael@theutz.com>

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,25 @@
-import fs from 'fs'
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import { homedir } from 'os';
-import { resolve } from 'path';
+import fs from "fs";
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import { homedir } from "os";
+import { resolve } from "path";
 
-let host = 'theutz.test';
+let host = "theutz.test";
 
 export default defineConfig({
     plugins: [
-        laravel([
-            'resources/css/app.css',
-            'resources/js/app.js',
-        ]),
+        laravel(["resources/css/app.css", "resources/js/app.js"]),
+        {
+            name: "blade",
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith(".blade.php")) {
+                    server.ws.send({
+                        type: "full-reload",
+                        path: "*",
+                    });
+                }
+            },
+        },
     ],
     server: detectServerConfig(host),
 });
@@ -28,7 +36,7 @@ function detectServerConfig(host) {
         host,
         https: {
             key: fs.readFileSync(keyPath),
-            cert: fs.readFileSync(crtPath)
-        }
-    }
+            cert: fs.readFileSync(crtPath),
+        },
+    };
 }
