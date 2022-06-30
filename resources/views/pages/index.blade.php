2093d2ccbccb6943dfdc36acbabdac4f74191beb
Michael Utz <michael@theutz.com>
2022-06-30 15:14:01 +0300

    Copy guest layout to index
    Signed-off-by: Michael Utz <michael@theutz.com>

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,2 +1,32 @@
 <x-guest-layout>
 </x-guest-layout>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1">
+    <meta name="csrf-token"
+          content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect"
+          href="https://cdn.fontshare.com"
+          crossOrigin="anonymous" />
+    <link rel="stylesheet"
+          href="https://api.fontshare.com/v2/css?f[]=satoshi@700,500,400&display=swap" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div class="font-sans antialiased text-gray-900">
+        {{ $slot }}
+    </div>
+</body>
+
+</html>
