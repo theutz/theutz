553e8c9c9370057e98425dd341334b4c6c1315e0
Michael Utz <michael@theutz.com>
2022-06-30 12:13:35 +0300

    Add meta slot in layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,9 +1,12 @@
+@props(['meta'])
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        {{ $meta }}
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
