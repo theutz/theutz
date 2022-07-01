aeb315253d10a10f15a9f0db59ca9af74330d5aa
Michael Utz <michael@theutz.com>
2022-07-02 00:09:40 +0300

    Add initial migration for settings

---
+++ app/Settings/GlobalContent.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Settings;
+
+use Spatie\LaravelSettings\Settings;
+
+class Globals extends Settings
+{
+    public string $title;
+    public string $description;
+    public string $bio;
+    public string $facebook;
+    public string $instagram;
+    public string $twitter;
+    public string $linkedin;
+
+    public static function group(): string
+    {
+        return 'globals';
+    }
+}
--- config/settings.php
+++ config/settings.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Settings\Globals;
+
 return [
 
     /*
@@ -7,7 +9,7 @@ return [
      * put them (manually) here.
      */
     'settings' => [
-
+        Globals::class
     ],
 
     /*
