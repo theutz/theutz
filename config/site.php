ddd21778dd99509fda20b40ef41f32f0bc7e5b20
Michael Utz <michael@theutz.com>
2022-07-03 18:32:20 +0300

    Add site config

---
+++ config/site.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Storage;
+
+return [
+    /**
+     * Large title text that appears in the sidebar.
+     */
+    'title' => 'Michael Utz',
+
+    /**
+     * Smaller description paragraph used as `<meta />` description, as well.
+     */
+    'description' => <<<END
+        I believe there's still a lot of good that Web2 can do for the world, and I'm on a mission to prove it.
+    END,
+
+    /**
+     * Slightly longer descriptive text that appears in the sidebar
+     */
+    'bio' => <<<END
+        I am an ambitious, self-taught web developer who lives at the intersection of good design, content,
+        and code. With a background and education in the arts and humanities, years of professional and
+        personal experience in coding, and a passion for creating useful products, I have a lot to offer any team
+        looking to build a great user experience on the web.
+    END,
+
+    'logo' => "AOZJSeB3i0hRzjs3u8tyt7iOvlRtIS-metaVGhlVXR6LnN2Zw==-.svg",
+
+    /**
+     * Collection of social and contact links
+     */
+    'links' => [
+        'facebook' => 'https://facebook.com/utzbaba',
+        'instagram' => 'https://www.instagram.com/michael_utz',
+        'twitter' => 'https://twitter.com/theutz',
+        'linkedin' => 'https://linkedin.com/in/theutz',
+        'email' => 'michael@theutz.com'
+    ]
+];
