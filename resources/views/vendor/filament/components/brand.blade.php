c224795e5a9ac8a46e03d5d0076f3cdae7095faf
Michael Utz <michael@theutz.com>
2023-04-07 10:52:19 +0300

    Remove entire project

--- resources/views/vendor/filament/components/brand.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="flex items-center gap-2">
-    <img src="{{ Storage::url(config('site.logo')) }}" alt="{{ config('site.title') }}" class="h-8 w-auto" />
-    <h1>
-        {{ config('site.title') }}
-    </h1>
-</div>
